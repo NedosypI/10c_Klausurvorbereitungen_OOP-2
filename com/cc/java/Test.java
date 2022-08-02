a58f0cf4cd28481b1123231bfeb073a0c7e234cd
NedosypI <irinanedosyp@gmx.de>
2022-08-02 08:02:05 +0200

    update

--- com/cc/java/Test.java
+++ com/cc/java/Test.java
@@ -5,14 +5,25 @@ public class Test {
     // Felder (fields) | Attribute, Eigenschaften
     // private --> Kapselung / encapsulation/ data hiding
 
-    private String name;
+    private String name; // 
     private int age;
 
-    public Test(String name, int age) {
+    /* Costum Konstuktor da er belieig anpassbar ist durch Parameter
+     * Konstruktor ist auch eine Methode 
+     * alle Methoden werden kleingeschrieben ausser 
+     * Konstruktor wird großgeschrieben 
+     */
+    public Test(String name, int age) { 
         this.name = name;
         this.age = age;
     }
-	// Sichtbarkeit | access (access modifiers)
+
+    /* default Konstruktor: 
+ * public Test (){
+ * }; 
+ */
+
+	// Sichtbarkeit | access (access modifiers) = Zugriffsmodifizierer
 	// public --> alle dürfen sehen/schreiben (+)
 	// private --> NUR die eigene Klasse / Objekt (-)
 	// protected --> NUR die eigene Klasse / Objekt + Subklassen (#)
